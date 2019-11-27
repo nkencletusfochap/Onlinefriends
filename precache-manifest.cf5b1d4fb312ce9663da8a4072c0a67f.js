78ce531e42dae32181bbbd79da5d7a4da09b957f
nkencletusfochap <39742739+nkencletusfochap@users.noreply.github.com>
2019-11-27 19:05:24 +0100

    Updates

---
+++ precache-manifest.cf5b1d4fb312ce9663da8a4072c0a67f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6fa1c62d297faa838b3cee64b24acf",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9f83556436d52f4501ed",
+    "url": "/robotfriends/static/css/main.41445b32.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robotfriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "9f83556436d52f4501ed",
+    "url": "/robotfriends/static/js/main.264ee51e.chunk.js"
+  },
+  {
+    "revision": "073b338190ec97f93ce1",
+    "url": "/robotfriends/static/js/runtime-main.0cebde30.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
